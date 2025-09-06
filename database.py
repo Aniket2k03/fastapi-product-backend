d6282fa963737640c8731e4b112bf19465ac3021
Aniket <aniketpawar112003@gmail.com>
2025-09-06 12:50:13 +0530

    Updated project with .env support and DB config changes

--- database.py
+++ database.py
@@ -1,6 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-db_url = "postgresql://postgres:aniket@localhost:5432/aniket"
+# Load environment variables from .env
+load_dotenv()
+
+db_url = os.getenv("DATABASE_URL")
+
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
